52c23f7f847ec6f703d62fafed6f0a51a819b57e
Mohamed waleed <Mohamed waleed@DESKTOP-0AUSUD8>
2024-07-15 17:15:13 +0300

    Add project files.

---
+++ array/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int n = Convert.ToInt32(Console.ReadLine());
+        string[] arr = Console.ReadLine().Split(' ');
+        int x = Convert.ToInt32(Console.ReadLine());
+
+        for (int i = 0; i < n; i++)
+        {
+            if (Convert.ToInt32(arr[i]) == x)
+            {
+                Console.WriteLine(i);
+                return;
+            }
+        }
+
+        Console.WriteLine(-1);
+    }
+}
